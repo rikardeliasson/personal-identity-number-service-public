68bb296bc708d3620d5ae6ec2800b39a0e9f7487
rikar <rikard.eliasson8@gmail.com>
2021-04-22 17:22:03 +0200

    Algorithm in place, verified pass and fail functionality, started on persistance error handling

--- src/main/java/se/kronansapotek/personalidentitynumberservice/PersonalIdentityNumberController.java
+++ src/main/java/se/kronansapotek/personalidentitynumberservice/PersonalIdentityNumberController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
